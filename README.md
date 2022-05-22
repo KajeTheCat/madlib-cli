bd50e119dba606aa3b206e4f23b674e8cda728c9
Jacob <jamsbury95@hotmail.com>
2022-05-21 17:23:03 -0700

    spent way too long

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 ## Author
 Jacob Amsbury
+Assisted by JJ Escandor and Roger Huba
 
 ## Summary
+Created a program that can read a specified folder file.
---
+++ madlib_cli/madlib.py
@@ -0,0 +1,20 @@
+
+
+def read_template(file):
+    with open(file) as f:
+        contents = f.read()
+        return contents
+
+
+def parse_template(sample):
+    pass
+
+
+def merge(text):
+    pass
+
+
+if __name__ == '__main__':
+    read_template()
+    parse_template()
+    merge()
