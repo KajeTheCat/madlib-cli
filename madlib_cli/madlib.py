e61dfb49a715f7afb07de16728a9ef71e3b1aa1e
Jacob <jamsbury95@hotmail.com>
2022-05-21 18:26:49 -0700

    passed 3 out of 4

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,17 +1,23 @@
 
-
 def read_template(file):
+    try:
+        print('Locating file')
+    except FileNotFoundError:
+        print('Could not find file')
     with open(file) as f:
         contents = f.read()
         return contents
+    
 
 
 def parse_template(sample):
-    pass
+    sample.split(' ')
 
 
-def merge(text):
-    pass
+
+def merge(text,filler):
+    text = text.format(*filler)
+    return text
 
 
 if __name__ == '__main__':
