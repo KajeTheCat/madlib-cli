fb6cdaa2e7803b23a0cf330f57b20e935bebef9a
Jacob <jamsbury95@hotmail.com>
2022-05-22 16:10:06 -0700

    finished

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,5 +1,7 @@
+import re
 
-def read_template(file):
+
+def read_template(file = 'assets/make_me_a_video_game_template.txt'):
     try:
         print('Locating file')
     except FileNotFoundError:
@@ -7,20 +9,53 @@ def read_template(file):
     with open(file) as f:
         contents = f.read()
         return contents
-    
 
 
-def parse_template(sample):
-    sample.split(' ')
+arr = []
 
+
+def parse_template(sample = read_template()):
+    pattern = r"[{}]"
+    pArr = []
+    strList = re.split(pattern,sample)
+    for str in strList:
+        if 'adjective' in str.lower():
+            arr.append(str)
+            pArr.append('{}')
+        elif 'noun' in str.lower():
+            arr.append(str)
+            pArr.append('{}')
+        elif 'a first name' in str.lower():
+            arr.append(str)
+            pArr.append('{}')
+        elif 'past tense verb' in str.lower():
+            arr.append(str)
+            pArr.append('{}')
+        elif 'animal' in str.lower():
+            arr.append(str)
+            pArr.append('{}')
+        elif 'a girl\'s name' in str.lower():
+            arr.append(str)
+            pArr.append('{}')
+        elif 'number' in str.lower():
+            arr.append(str)
+            pArr.append('{}')
+        else:
+            pArr.append(str)
+    return [("".join(pArr)), (tuple(arr))]
 
 
 def merge(text,filler):
     text = text.format(*filler)
     return text
 
+def userInput():
+    templst = []
+    for a in arr:
+        response = input('Give me a(n) ' + a + ': ')
+        templst.append(response)
+    return templst
+
 
 if __name__ == '__main__':
-    read_template()
-    parse_template()
-    merge()
+    print(merge(parse_template()[0],userInput()))
